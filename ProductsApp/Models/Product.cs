752302b25b77ad2425947ae6aa87095d8b9d9a61
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 11:22:53 +0200

    model proizvoda

--- ProductsApp/Models/Product.cs
+++ ProductsApp/Models/Product.cs
@@ -7,5 +7,9 @@ namespace ProductsApp.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
     }
 }
